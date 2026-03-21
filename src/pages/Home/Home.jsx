7b1d4d9c369be8a75e15a682fd980d79d42e8cec
Minura Jayasingha <minurajayasingha@gmail.com>
2026-03-21 22:03:33 +0530

    feat:Add TopTrends sectionn to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import HeroBanner from './sections/HeroBanner'
 import FeaturesSection from './sections/FeaturesSection'
 import CTASection from './sections/CTASection'
+import TopTrends from './sections/TopTrends'
 
 export default function Home() {
   return (
     <div>
       <HeroBanner />
+      <TopTrends />
       <FeaturesSection />
       <CTASection />
     </div>
