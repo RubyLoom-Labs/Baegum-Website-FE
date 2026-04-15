0c44226a63ff60120db291ffb80414335ffa93c6
MinuraJayasingha <minurajayasingha@gmail.com>
2026-04-15 17:41:49 +0530

    feat:add Home sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import PromoBanners from './sections/PromoBanners'
 import VideoBanner from './sections/Videobanner'
 import CategoryShowcase from './sections/CategoryShowcase'
 import FeaturedProductSection from "./sections/FeaturedProductSection";
+import ProdcutTabs from "./sections/ProductTabs";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <TopTrends />
       <PromoBanners />
       <FeaturedProductSection />
+      <ProdcutTabs />
       <VideoBanner />
       <CategoryShowcase />
     </div>
